1fa7b89a867d94eb7939e4fa4c8cbb84a5f051fd
Vitalii Vitaminenko <vitamin@ukr.net>
2019-05-11 22:46:07 +0300

    Add meh again

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
-var lvivRelocationDate = new Date("1998-08-01");
-var todaysDate = new Date();
-var monthsLivingInLviv = todaysDate.getMonth() - lvivRelocationDate.getMonth() + (12 * (todaysDate.getFullYear() - lvivRelocationDate.getFullYear()));
+const lvivRelocationDate = new Date("1998-08-01");
+const todaysDate = new Date();
+const monthsLivingInLviv = todaysDate.getMonth() - lvivRelocationDate.getMonth() + (12 * (todaysDate.getFullYear() - lvivRelocationDate.getFullYear()));
 document.getElementById("kyiv-rent-months").innerHTML = "paid " + monthsLivingInLviv + " month" + (monthsLivingInLviv === 1 ? "" : "s") + " of rent already";
 function switchToColor(color) {
     try {
@@ -17,10 +17,10 @@ document.getElementById("chartreuse").addEventListener("click", switchToColor.bi
 document.getElementById("tomato").addEventListener("click", switchToColor.bind(this, "tomato"));
 document.getElementById("royalblue").addEventListener("click", switchToColor.bind(this, "royalblue"));
 document.getElementById("meh").addEventListener("click", function() {
-    this.childNodes[2].play();
+    this.querySelector('audio').play();
 });
 function replaceNoscript(noscript) {
-    var parent = noscript.parentElement;
+    const parent = noscript.parentElement;
     Array.prototype.slice.call((new DOMParser()).parseFromString(noscript.textContent, "text/html").body.children).forEach(function(element) {
         parent.insertBefore(element, noscript);
     });
@@ -31,7 +31,7 @@ function callFuncOnCollection(collection, func) {
         func(item);
     });
 }
-var childhoodPhotos = document.getElementById("childhood").getElementsByClassName("photos")[0];
+const childhoodPhotos = document.getElementById("childhood").getElementsByClassName("photos")[0];
 document.getElementById("resume").addEventListener("click", function() {
     callFuncOnCollection(childhoodPhotos.getElementsByTagName("NOSCRIPT"), replaceNoscript);
 });
