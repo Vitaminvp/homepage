c24b1dc13fb8dbf128c3e82e57254d1ce8be9b93
vitaminvp <vitamin@ukr.net>
2019-08-24 10:35:55 +0300

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,6 +21,7 @@ document.getElementById("tomato").addEventListener("click", switchToColor.bind(t
 document.getElementById("royalblue").addEventListener("click", switchToColor.bind(this, "royalblue"));
 document.getElementById("deeppink").addEventListener("click", switchToColor.bind(this, "deeppink"));
 document.getElementById("aqua").addEventListener("click", switchToColor.bind(this, "aqua"));
+document.getElementById("red").addEventListener("click", switchToColor.bind(this, "red"));
 document.getElementById("meh").addEventListener("click", function() {
     this.querySelector('audio').play();
 });
